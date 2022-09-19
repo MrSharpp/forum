83a969d1da9b6b874d238c806b812f29d6e9221c
amiralam0 <sharpprogrammer2018@gmail.com>
2022-09-19 10:05:50 -0700

    authentication

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,49 +1,71 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { schema } from '@ioc:Adonis/Core/Validator'
 import Database from '@ioc:Adonis/Lucid/Database'
+import Hash from '@ioc:Adonis/Core/Hash'
 
 export default class UsersController {
-  /**
-   * @swagger
-   * /api/users:
-   * post:
-   *     tags:
-   *       - Users
-   *     requestBody:
-   *       required: true
-   *       content:
-   *         application/json:
-   *           description: User payload
-   *           schema:
-   *             type: object
-   *             properties:
-   *               phone:
-   *                 type: string
-   *                 example: 'James Bond'
-   *                 required: true
-   *               email:
-   *                 type: string
-   *                 example: 'Bond007@example.com'
-   *                 required: true
-   *     produces:
-   *       - application/json
-   *     responses:
-   *       200:
-   *         description: Success
-   *         content:
-   *           application/json:
-   *             schema:
-   *               $ref: '#/components/schemas/User'
-   */
+  public async login({ request, response }) {
+    const loginSchema = schema.create({
+      email: schema.string(),
+      password: schema.string(),
+    })
 
-  public async login(ctx: HttpContextContract) {
-    return Database.from('users').select('*')
+    let payload
+
+    try {
+      payload = await request.validate({
+        schema: loginSchema,
+        message: {
+          required: 'The {{ field }} is required to create a new account',
+        },
+      })
+    } catch (err) {
+      return response.send({ status: err.status, msg: err.message })
+    }
+
+    const user = await Database.from('users').where('email', payload.email)
+    console.log(user)
+    const hashedPassword = await Hash.make(payload.password)
+    if (!user || !(await Hash.verify(hashedPassword, payload.password)))
+      return response.send({ status: 401, msg: 'Invalid Login Credentials' })
+
+    return response.send({ staus: 200, msg: 'You have an account' })
   }
 
-  public async signup(ctx: HttpContextContract) {
-    return Database.table('users').insert({
-      username: 'admin',
-      password: 'password',
-      email: 'sharpprogrammer2018@gmail.com',
+  public async signup({ request, response }) {
+    const newUserSchema = schema.create({
+      email: schema.string(),
+      password: schema.string(),
+      username: schema.string(),
     })
+
+    let payload
+
+    try {
+      payload = await request.validate({
+        schema: newUserSchema,
+        message: {
+          required: 'The {{ field }} is required to create a new account',
+        },
+      })
+    } catch (err) {
+      return response.send({ status: err.status, msg: err.messages })
+    }
+
+    // validate if users exists already or not
+    await Database.from('users').where('email', payload.email)
+
+    await Database.table('users')
+      .insert({
+        username: payload.username,
+        password: await Hash.make(payload.password),
+        email: payload.email,
+      })
+      .then(() => {
+        response.send({ status: 200, msg: 'Account created, Sucessfully!' })
+      })
+      .catch((err) => {
+        console.log(`error when inserting a user: ${err}`)
+        response.send({ status: 409, msg: 'Account created, Sucessfully!' })
+      })
   }
 }
