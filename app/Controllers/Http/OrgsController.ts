77bfb9074deb0bfa3f8d18126785eddf84a8fe1b
amiralam0 <sharpprogrammer2018@gmail.com>
2022-09-24 19:29:52 +0530

    organisation

--- app/Controllers/Http/OrgsController.ts
+++ app/Controllers/Http/OrgsController.ts
@@ -1,3 +1,25 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Organization from 'App/Models/Organization'
+import { schema } from '@ioc:Adonis/Core/Validator'
 
-export default class OrgsController {}
+export default class OrgsController {
+  public async index({ response }) {
+    const organisations = await Organization.all()
+    response.status(200).send({ organisations })
+  }
+
+  public async create({ request, response }) {
+    const orgSchema = schema.create({ orgName: schema.string() })
+
+    let payload = request.all()
+    await request.validate({ schema: orgSchema, msg: 'All field are required!' })
+
+    const organisation = new Organization()
+    console.log(payload.orgName)
+    organisation.orgName = payload.orgName
+    organisation.moderatorsId = []
+
+    await organisation
+      .save()
+      .then((resp) => response.status(200).send({ msg: 'Organisation Created Succesfully!' }))
+  }
+}
--- app/Models/Organization.ts
+++ app/Models/Organization.ts
@@ -1,13 +1,24 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, beforeSave, column } from '@ioc:Adonis/Lucid/Orm'
 
 export default class Organization extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @column({ isPrimary: false })
+  public orgName: string
+
+  @column({ isPrimary: false, serialize: true })
+  public moderatorsId: Array<string
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  // @beforeSave()
+  // public static async stringify(organisation: Organization) {
+  //   organisation.moderatorsId = organisation.moderatorsId.toString()
+  // }
 }
